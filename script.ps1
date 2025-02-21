6a054877b70702911e26e9ea06ede67dc25bfbe3
Carlos Velasquez <velasqezc@gmail.com>
2025-02-21 11:19:15 +0100

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,3 +1,16 @@
 $ImageUrl = "https://raw.githubusercontent.com/crlosvelasquez/mm_image/refs/heads/main/fondo.jpg"
 $DestinationPath = "C:\Windows\Web\Wallpaper\mainmemory\background.jpg"
-Invoke-WebRequest -Uri $ImageUrl -OutFile $DestinationPath
+$DestinationFolder = Split-Path -Path $DestinationPath -Parent
+
+# Verificar si la carpeta de destino existe, si no, crearla
+if (!(Test-Path $DestinationFolder)) {
+    New-Item -ItemType Directory -Path $DestinationFolder -Force | Out-Null
+}
+
+# Descargar la imagen con manejo de errores
+try {
+    Invoke-WebRequest -Uri $ImageUrl -OutFile $DestinationPath
+    Write-Output "Descarga completada: $DestinationPath"
+} catch {
+    Write-Output "Error al descargar la imagen: $_"
+}
